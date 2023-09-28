9921c4a5c17c6a578b5965d7df50f6efc5a903dc
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-09-28 22:24:40 +0300

    add new array namesWithAges

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const names = ['Семен', 'Иван', 'Петр', 'Татьяна']
 const ages = [18, 27, 74, 34]
 
+const namesWithAges = []
+namesWithAges.length = 4
+
+for (let i = 0; i < namesWithAges.length; i++) {
+    namesWithAges[i] = names[i] + " "  + ages[i] + " лет/год(а/ов)"
+    console.log(namesWithAges[i]);
+}
